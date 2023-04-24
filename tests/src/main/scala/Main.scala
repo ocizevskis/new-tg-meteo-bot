eb59be5f536e0ec690350cb8d987125d690147ed
ocizevskis <115380988+ocizevskis@users.noreply.github.com>
2023-04-24 17:18:09 +0300

    Update Main.scala

--- tests/src/main/scala/Main.scala
+++ tests/src/main/scala/Main.scala
@@ -17,7 +17,7 @@ def send_notif(i: UserRiver) =
   Dati pēdējoreiz atjaunināti ${i.date}"""
   val message = Map("chat_id" -> i.chatid.toString, "text" -> text)
 
-  requests.post("https://api.telegram.org/bot5637989500:AAFBu3aHNzAlifMwUrTA5zlIcQIMM670AMo/sendMessage", data = message)
+  requests.post("https://api.telegram.org/<token>/sendMessage", data = message)
   
 // def get_data(path: String): List[UserRiver] = 
 //     val ds = SQLiteDataSource()
